93d6b429084873073f1e73b7aa2d14786e5fc930
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-01-26 14:49:12 -0300

    refactor directory and import style minified

--- src/app.js
+++ src/app.js
@@ -9,10 +9,14 @@ require('./models/db/database')
 const app = express()
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+//views
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
 app.use('/', rootRouter)
+
+//routes
 app.use('/checklists', checklistRouter)
 
 app.listen(3333, () => {
