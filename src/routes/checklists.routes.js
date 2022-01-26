a49b0684373ba28b36f209d5770c81317b0b8999
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-01-26 08:31:34 -0300

    create checklist routes - conversing with mongodb succesfully

--- src/routes/checklists.routes.js
+++ src/routes/checklists.routes.js
@@ -2,19 +2,65 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (request, response) => {
-  console.log('ola checklists')
-  response.send()
+const Checklist = require('../models/repositories/checklist')
+
+router.post('/', async (request, response) => {
+  const { name } = request.body
+
+  try {
+    const checklist = await Checklist.create({ name })
+    response.status(201).json(checklist)
+  }
+
+  catch (error) {
+    response.status(422).json(error)
+  }
 })
 
-router.get('/:id', (request, response) => {
-  console.log(request.params.id)
-  response.send(`ID: ${request.params.id}`)
+router.get('/', async (request, response) => {
+  try {
+    const checklist = await Checklist.find({})
+    response.status(200).json(checklist)
+  }
+  
+  catch (error) {
+    response.status(500).json(error)
+  }
 })
 
-router.post('/', (request, response) => {
-  console.log(request.body)
-  response.status(201).json(request.body)
+router.get('/:id', async (request, response) => {
+  try {
+    const checklists = await Checklist.findById(request.params.id)
+    response.status(200).json(checklists)
+  }
+  
+  catch (error) {
+    response.status(422).json(error)
+  }
+})
+
+router.put('/:id', async (request, response) => {
+  const { name } = request.body
+
+  try {
+    const checklist = await Checklist.findByIdAndUpdate(request.params.id, { name }, { new: true })
+    response.status(200).json(checklist)
+  }
+  
+  catch (error) {
+    response.status(422).json(error)
+  }
+})
+
+router.delete('/:id', async (request, response) => {
+  try {
+    const checklist = await Checklist.findByIdAndRemove(request.params.id)
+    response.status(200).json(checklist)
+  }
+  
+  catch (error) {
+    response.status(422).json(error)
+  }
 })
 
 module.exports = router
